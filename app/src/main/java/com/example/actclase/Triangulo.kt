5254c4ceb4375d639ce0d52c936665db10595c6e
EAG <mataramosmiguel@gmail.com>
2023-10-24 17:23:33 +0200

    clase triangulillo sin warnin yuju

--- app/src/main/java/com/example/actclase/Triangulo.kt
+++ app/src/main/java/com/example/actclase/Triangulo.kt
@@ -2,8 +2,13 @@ package com.example.actclase
 
 class Triangulo (var lado1 : Double,var lado2 : Double,var lado3 : Double){
     var perimetro : Double = lado1 + lado2 + lado3
-    lateinit var tipo : String
+    var tipo : String = when {
+        lado1 == lado2 && lado2 == lado3 -> "Equilatero"
+        lado1 == lado2 || lado2 == lado3 -> "Isosceles"
+        else -> "Escaleno"
+    }
 
+    /*
     fun tipoTriangulo(){
         if (lado1 == lado2 && lado2 == lado3){
             tipo = "Equilatero"
@@ -13,7 +18,7 @@ class Triangulo (var lado1 : Double,var lado2 : Double,var lado3 : Double){
             tipo =  "Escaleno"
         }
     }
-
+     */
 
 
 
