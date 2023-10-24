cca703cec80c29cccdd98a94161f9fcf13f47f63
EAG <mataramosmiguel@gmail.com>
2023-10-24 17:41:39 +0200

    clase triangulillo completa

--- app/src/main/java/com/example/actclase/Triangulo.kt
+++ app/src/main/java/com/example/actclase/Triangulo.kt
@@ -2,6 +2,7 @@ package com.example.actclase
 
 class Triangulo (var lado1 : Double,var lado2 : Double,var lado3 : Double){
     var perimetro : Double = lado1 + lado2 + lado3
+
     var tipo : String = when {
         lado1 == lado2 && lado2 == lado3 -> "Equilatero"
         lado1 == lado2 || lado2 == lado3 -> "Isosceles"
@@ -20,6 +21,11 @@ class Triangulo (var lado1 : Double,var lado2 : Double,var lado3 : Double){
     }
      */
 
+    fun esRectangulo():Boolean{
+        val lados = listOf(lado1, lado2, lado3)
+        lados.sorted()
 
+        return (lados[0] * lados[0] + lados[1] * lados[1] == lados[2] * lados[2])
+    }
 
 }
