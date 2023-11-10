26fa5b7b24843a84b55f5ffafe9c35466947e038
EAG <mataramosmiguel@gmail.com>
2023-11-10 20:53:56 +0100

    ej12 hecho

--- app/src/main/java/com/example/actclase/Circunferencia.kt
+++ app/src/main/java/com/example/actclase/Circunferencia.kt
@@ -1,15 +1,23 @@
 package com.example.actclase
 
-//class Circunferencia(val radio:Double) {
-//
-//    fun Circunferencia()
-//
-//    fun longitud():Double{
-//        return 2*3.1416*radio
-//    }
-//
-//    @Override
-//    override fun toString(): String {
-//        return "Su radio es de "+longitud()
-//    }
-//}
+import kotlin.math.PI
+
+class Circunferencia(var radio:Double) {
+    var centro = mutableListOf<Int>(0,0)
+
+    constructor(radio: Double, centro:MutableList<Int>, punto:MutableList<Int>):this(radio){
+        this.centro = centro
+
+        this.radio = ((punto[0]-centro[0])*(punto[0]-centro[0]) + (punto[1] - centro[1]) * (punto[1] - centro[1])).toDouble()
+
+    }
+
+    fun longitud():Double{
+        return 2 * PI * radio
+    }
+
+    @Override
+    override fun toString(): String {
+        return "Su radio es de "+longitud()
+    }
+}
